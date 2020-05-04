digital-blueprint/esign-app

commit 5dbb3033b4c37ebfb5810d78ed9522047217490d

Show a loading spinner between login and auth checks
In case the activity gets loaded and we are already logged in there
is a short perio where we still wait for the person fetch to finish
so we can see if the user has the right permissions.

Instead of showing the "not logged in" message show a loading spinner.

src/vpu-qualified-signature-pdf-upload.js
@@ -504,6 +504,9 @@ class QualifiedSignaturePdfUpload extends ScopedElementsMixin(VPUSignatureLitEle
     }
 
     render() {
+        if (this.isLoading()) {
+            return html`<vpu-mini-spinner></vpu-mini-spinner>`;
+        }
         return html`
             <div class="${classMap({hidden: !this.isLoggedIn() || !this.hasSignaturePermissions()})}">
                 <div class="field">

src/vpu-official-signature-pdf-upload.js
@@ -293,6 +293,9 @@ class OfficialSignaturePdfUpload extends ScopedElementsMixin(VPUSignatureLitElem
     }
 
     render() {
+        if (this.isLoading()) {
+            return html`<vpu-mini-spinner></vpu-mini-spinner>`;
+        }
         return html`
             <div class="${classMap({hidden: !this.isLoggedIn() || !this.hasSignaturePermissions()})}">
                 <div class="field">

src/vpu-signature-lit-element.js
@@ -12,16 +12,18 @@ export default class VPUSignatureLitElement extends LitElement {
     }
 
     _updateAuth() {
-        if (this.isLoggedIn() && !this._loginCalled) {
-            this._loginCalled = true;
-            this.loginCallback();
+        // Every time isLoggedIn()/isLoading() return something different we request a re-render
+        let newLoginState = [this.isLoggedIn(), this.isLoading()];
+        if (this._loginState.toString() !== newLoginState.toString()) {
+            this.requestUpdate();
         }
+        this._loginState = newLoginState;
     }
 
     connectedCallback() {
         super.connectedCallback();
 
-        this._loginCalled = false;
+        this._loginState = [];
         this._subscriber = new events.EventSubscriber('vpu-auth-update', 'vpu-auth-update-request');
         this._updateAuth = this._updateAuth.bind(this);
         this._subscriber.subscribe(this._updateAuth);
@@ -38,9 +40,8 @@ export default class VPUSignatureLitElement extends LitElement {
         return (window.VPUPerson !== undefined && window.VPUPerson !== null);
     }
 
-    loginCallback() {
-        // Implement in subclass
-        this.requestUpdate();
+    isLoading() {
+        return (!this.isLoggedIn() && window.VPUAuthToken !== undefined);
     }
 
     getOrganization() {

commit d465e9cba47c42165aee6a85c1cd6d6cfcf3d8e3

Add hotfix for broken upload from commit 5dbb3033b4c37ebfb5810d78ed9522047217490d (#4)

src/vpu-qualified-signature-pdf-upload.js
@@ -512,7 +512,8 @@ class QualifiedSignaturePdfUpload extends ScopedElementsMixin(VPUSignatureLitEle
 
     render() {
         if (this.isLoading()) {
-            return html`<vpu-mini-spinner></vpu-mini-spinner>`;
+            // TODO: breaks upload (added in commit 5dbb3033b4c37ebfb5810d78ed9522047217490d)
+            // return html`<vpu-mini-spinner></vpu-mini-spinner>`;
         }
         return html`
             <div class="${classMap({hidden: !this.isLoggedIn() || !this.hasSignaturePermissions()})}">