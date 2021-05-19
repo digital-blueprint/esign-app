57ca9a16f05a3ff30d604309572b4c952f239a5f
Christoph Reiter <reiter.christoph@gmail.com>
2021-05-19 15:11:02 +0200

    Move the base element into its own file

---
+++ src/base-element.js
@@ -0,0 +1,62 @@
+import {AdapterLitElement} from "@dbp-toolkit/provider/src/adapter-lit-element";
+
+export class BaseLitElement extends AdapterLitElement {
+    constructor() {
+        super();
+        this.auth = {};
+    }
+
+    static get properties() {
+        return {
+            ...super.properties,
+            auth: { type: Object },
+        };
+    }
+
+    _(selector) {
+        return this.shadowRoot === null ? this.querySelector(selector) : this.shadowRoot.querySelector(selector);
+    }
+
+    _hasSignaturePermissions(roleName) {
+        return (this.auth.person && Array.isArray(this.auth.person.roles) && this.auth.person.roles.indexOf(roleName) !== -1);
+    }
+
+    _updateAuth() {
+        this._loginStatus = this.auth['login-status'];
+        // Every time isLoggedIn()/isLoading() return something different we request a re-render
+        let newLoginState = [this.isLoggedIn(), this.isLoading()];
+        if (this._loginState.toString() !== newLoginState.toString()) {
+            this.requestUpdate();
+        }
+        this._loginState = newLoginState;
+    }
+
+    update(changedProperties) {
+        changedProperties.forEach((oldValue, propName) => {
+            switch (propName) {
+                case "auth":
+                    this._updateAuth();
+                    break;
+            }
+        });
+
+        super.update(changedProperties);
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+
+        this._loginStatus = '';
+        this._loginState = [];
+    }
+
+    isLoggedIn() {
+        return (this.auth.person !== undefined && this.auth.person !== null);
+    }
+
+    isLoading() {
+        if (this._loginStatus === "logged-out")
+            return false;
+        return (!this.isLoggedIn() && this.auth.token !== undefined);
+    }
+}
