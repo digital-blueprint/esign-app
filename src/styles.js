5c90dd0d6261eb9d9efb9acb22d86aa18c348024
Tamara Steinwender <tamara.steinwender@tugraz.at>
2021-07-06 15:07:48 +0200

    Update Submodule

---
+++ src/styles.js
@@ -0,0 +1,10 @@
+import {css} from 'lit-element';
+
+export function getSignatureCss() {
+    // language=css
+    return css`
+        #pdf-preview {
+            position: sticky;
+            top: 0px;
+        }`;
+}
