8607768c71b578fe8b65686d9eb311cef6cb0739
Christoph Reiter <reiter.christoph@gmail.com>
2023-03-07 11:12:43 +0100

    Port to glob v9

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import path from 'path';
 import url from 'url';
-import glob from 'glob';
+import {globSync} from 'glob';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import copy from 'rollup-plugin-copy';
@@ -94,7 +94,7 @@ export default (async () => {
                       'src/dbp-signature-verification.js',
                       'src/dbp-signature-verification-full.js',
                   ]
-                : glob.sync('test/**/*.js'),
+                : globSync('test/**/*.js'),
         output: {
             dir: 'dist',
             entryFileNames: '[name].js',
