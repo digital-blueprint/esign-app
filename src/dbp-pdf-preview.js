6ee8db8ff3264f706ee4c215ea84684520a8c827
Christoph Reiter <reiter.christoph@gmail.com>
2024-05-21 13:13:35 +0200

    pdf-preview: explicitely disable isEvalSupported
    This disables eval even if the CSP isn't active, and also
    removes the console error if the CSP is active.

--- src/dbp-pdf-preview.js
+++ src/dbp-pdf-preview.js
@@ -246,7 +246,7 @@ export class PdfPreview extends ScopedElementsMixin(DBPLitElement) {
 
         // get handle of pdf document
         try {
-            this.pdfDoc = await pdfjs.getDocument({data: data}).promise;
+            this.pdfDoc = await pdfjs.getDocument({data: data, isEvalSupported: false}).promise;
         } catch (error) {
             console.error(error);
             this.showErrorMessage = true;
