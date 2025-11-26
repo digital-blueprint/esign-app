7a2a550629bb040885b06d658268ded53eb83a91
Christoph Reiter <reiter.christoph@gmail.com>
2025-11-26 13:44:51 +0100

    Delete some unused code

--- src/utils.js
+++ src/utils.js
@@ -24,10 +24,6 @@ export const findObjectInApiResults = (identifier, results, identifierAttribute 
     }
 };
 
-export const getPDFFileBase64Content = (file) => {
-    return file.contentUrl.replace(/data:\s*application\/pdf;\s*base64,/, '');
-};
-
 export const convertDataURIToBinary = (dataURI) => {
     const BASE64_MARKER = ';base64,';
     const base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
@@ -48,16 +44,6 @@ export const getDataURIContentType = (dataURI) => {
     const base64Index = dataURI.indexOf(BASE64_MARKER);
 
     return dataURI.substring(5, base64Index);
-};
-
-export const baseName = (str) => {
-    let base = String(str).substring(str.lastIndexOf('/') + 1);
-
-    if (base.lastIndexOf('.') !== -1) {
-        base = base.substring(0, base.lastIndexOf('.'));
-    }
-
-    return base;
 };
 
 export const fabricjs2pdfasPosition = (data) => {
