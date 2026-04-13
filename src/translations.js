6ce6ff548938d8163e6ee157744cf0c3f52e42db
Manuel Kocher <manuelkocher98@gmail.com>
2026-04-13 10:31:23 +0200

    Add new translations without usage for now

--- src/translations.js
+++ src/translations.js
@@ -3,12 +3,19 @@
 This is just needed to register the translation keys with i18next-cli
 
 These translations are used in the profile selection dbp-select which generates the key dynamically based on the profile names
+t('official-pdf-upload.profile-advanced-invisible-inspection-report')
+t('official-pdf-upload.profile-advanced-recognitions')
+t('official-pdf-upload.profile-advanced-SAP-orders')
+t('official-pdf-upload.profile-advanced-seal')
+t('official-pdf-upload.profile-advanced-official')
+t('official-pdf-upload.profile-advanced-invoices')
+t('official-pdf-upload.profile-advanced-english-invoices')
+t('official-pdf-upload.profile-advanced-official-CO')
+t('official-pdf-upload.profile-advanced-invisible-hr')
+t('official-pdf-upload.profile-advanced-rector')
 t('official-pdf-upload.profile-advanced')
 t('official-pdf-upload.profile-advanced-english')
 t('official-pdf-upload.profile-advanced-invisible')
-t('official-pdf-upload.profile-advanced-official')
-t('official-pdf-upload.profile-advanced-official-CO')
-t('official-pdf-upload.profile-advanced-rector')
 t('official-pdf-upload.profile-official')
 
  */
