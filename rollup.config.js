0472b9442403731a370825eab41c35ce8d54b6fd
Patrizio Bekerle <patrizio@bekerle.com>
2026-05-21 12:39:05 +0200

    rollup: show build time
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- rollup.config.js
+++ rollup.config.js
@@ -430,6 +430,12 @@ Dependencies:
                       },
                   })
                 : false,
+            watch && {
+                name: 'log-rebuild',
+                writeBundle() {
+                    console.log(`\n[${new Date().toLocaleString()}] dist rebuilt\n`);
+                },
+            },
         ],
     };
 })();
